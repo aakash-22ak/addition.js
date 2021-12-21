3c9e321eb7c8c322590eeac32471c4b85fcfbd0e
aakash-22ak <akashshrestha678@gmail.com>
2021-12-21 08:52:07 +0530

    first commit

---
+++ simpleadd.js
@@ -0,0 +1,9 @@
+function myfunction(){
+  let num1= document.getElementById('num1').value;
+  let num2= document.getElementById('num2').value;
+  let addition= Number(num1)+Number(num2);
+  
+  document.getElementById('sum').value= addition;
+
+ 
+}
